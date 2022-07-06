708e37c623c939d1439d6596110c882813d52a2b
tymv <tumkir@ya.ru>
2022-07-06 18:47:18 +0300

    Удаляем индекс перед тестом и заполняем заново тестовыми данными

--- src/tests/functional/utils/es_loader.py
+++ src/tests/functional/utils/es_loader.py
@@ -2,8 +2,9 @@ from elasticsearch.helpers import async_bulk
 
 
 async def create_index(es_client, index_name: str, index: dict) -> None:
-    if not await es_client.indices.exists(index=index_name):
-        await es_client.indices.create(index=index_name, body=index)
+    if await es_client.indices.exists(index=index_name):
+        await es_client.indices.delete(index=index_name)
+    await es_client.indices.create(index=index_name, body=index)
 
 
 def generate_data(index_name: str, data: list[dict]) -> list[dict]:
