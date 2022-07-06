978b8cdedc971184b5f4d61bf6a32e0343591516
tymv <tumkir@ya.ru>
2022-07-06 17:33:34 +0300

    Тесты эндоинта films

---
+++ src/tests/functional/src/test_films.py
@@ -0,0 +1,69 @@
+import asyncio
+import random
+from uuid import uuid4
+
+import pytest
+from test_data.movies_data import MOVIES
+
+
+@pytest.mark.asyncio
+async def test_random_existent_film(load_movies_data, make_get_request):
+    random_film = MOVIES[random.randint(0, len(MOVIES)-1)]
+
+    response = await make_get_request(f'/films/{random_film["uuid"]}')
+
+    assert response.status == 200
+    assert response.body == random_film
+
+
+@pytest.mark.asyncio
+async def test_random_nonexistent_film(load_movies_data, make_get_request):
+    nonexistent_film_id = uuid4()
+
+    response = await make_get_request(f'/films/{nonexistent_film_id}')
+
+    assert response.status == 404
+    assert len(response.body) == 1
+    assert response.body == {"detail": "Film not found"}
+
+
+@pytest.mark.asyncio
+async def test_films_query_params_type_error(load_movies_data, make_get_request):
+    response = await make_get_request('/films/', {"from": "f", "size": "s", "page": "k"})
+
+    assert response.status == 422
+    assert response.body == {"detail": [
+        {"loc": ["query", "from"], "msg":"value is not a valid integer", "type": "type_error.integer"},
+        {"loc": ["query", "size"], "msg":"value is not a valid integer", "type": "type_error.integer"},
+        {"loc": ["query", "page"], "msg":"value is not a valid integer", "type": "type_error.integer"}
+        ]}
+
+
+@pytest.mark.asyncio
+async def test_films_search(load_movies_data, make_get_request):
+    film_to_find = [film for film in MOVIES if film['uuid'] == 'ee580283-32db-4181-9f11-bd9d171944c3']
+    await asyncio.sleep(1)
+
+    response = await make_get_request('/films/', {'query': 'Endor'})
+
+    assert response.status == 200
+    assert response.body != []
+    assert response.body == film_to_find
+
+
+@pytest.mark.asyncio
+async def test_films_endpoint(load_movies_data, make_get_request):
+    await asyncio.sleep(1)
+    response = await make_get_request('/films/', {})
+
+    assert response.status == 200
+    assert len(response.body) == len(MOVIES)
+    assert response.body == MOVIES
+
+# @pytest.mark.asyncio
+# async def test_without_index(clear_redis_cashe, make_get_request):
+#     response = await make_get_request('/films')
+
+#     assert response.status == 200
+#     assert len(response.body) == len(MOVIES)
+#     assert response.body == MOVIES
---
+++ src/tests/functional/test_data/movies_data.py
@@ -0,0 +1,549 @@
+MOVIES: list[dict] = [
+    {
+      "uuid": "73ecd1e6-6326-405a-b51b-69008f383b72",
+      "title": "Lego Star Wars: The Complete Saga",
+      "imdb_rating": 8.5,
+      "description": "In Lego Star Wars: The Complete Saga, you can now experience all 6 episodes at once. Containing the same levels and locations. Only with newer features and improvements. Such as more extras to unlock, more vehicles to build, new playable characters, & a better customizing Lego character feature. Now all Lego & Star Wars fans alike can experience this game more than it's successors.",
+      "genre": [
+        {
+          "name": "Action",
+          "uuid": "3d8d9bf5-0d90-4353-88ba-4ccc5d2c07ff"
+        },
+        {
+          "name": "Adventure",
+          "uuid": "120a21cf-9097-479e-904a-13dd7198c1dd"
+        },
+        {
+          "name": "Comedy",
+          "uuid": "5373d043-3f41-4ea8-9947-4b746c601bbd"
+        }
+      ],
+      "actors": [
+        {
+          "uuid": "746b394f-7808-4386-a281-b06504c07b58",
+          "full_name": "Ahmed Best"
+        },
+        {
+          "uuid": "86f1f44b-39f5-41a6-8b3b-af5a9ed09858",
+          "full_name": "Anthony Daniels"
+        },
+        {
+          "uuid": "c5664b88-7286-4b38-9a3c-3f3cc6f72e9e",
+          "full_name": "Warwick Davis"
+        },
+        {
+          "uuid": "e039eedf-4daf-452a-bf92-a0085c68e156",
+          "full_name": "Peter Cushing"
+        }
+      ],
+      "writers": [
+        {
+          "uuid": "a5a8f573-3cee-4ccc-8a2b-91cb9f55250a",
+          "full_name": "George Lucas"
+        }
+      ],
+      "directors": [
+        {
+          "uuid": "b505845f-d7bc-4271-a59d-5dc16a641cac",
+          "full_name": "Jon Burton"
+        }
+      ]
+    },
+    {
+      "uuid": "daae47e4-cbd0-4ffd-a150-55201b357d5b",
+      "title": "Lego Star Wars: The Video Game",
+      "imdb_rating": 8.2,
+      "description": "Play through the star wars prequel trilogy in episodes 1,2,and 3, in lego.",
+      "genre": [
+        {
+          "name": "Action",
+          "uuid": "3d8d9bf5-0d90-4353-88ba-4ccc5d2c07ff"
+        },
+        {
+          "name": "Adventure",
+          "uuid": "120a21cf-9097-479e-904a-13dd7198c1dd"
+        },
+        {
+          "name": "Comedy",
+          "uuid": "5373d043-3f41-4ea8-9947-4b746c601bbd"
+        }
+      ],
+      "actors": [
+        {
+          "uuid": "509c1168-0ced-4704-a3b9-f17b9dc37667",
+          "full_name": "Lindsay Duncan"
+        },
+        {
+          "uuid": "62df10e8-244d-4c31-b396-564dfbc2f9c5",
+          "full_name": "Hayden Christensen"
+        },
+        {
+          "uuid": "746b394f-7808-4386-a281-b06504c07b58",
+          "full_name": "Ahmed Best"
+        },
+        {
+          "uuid": "86f1f44b-39f5-41a6-8b3b-af5a9ed09858",
+          "full_name": "Anthony Daniels"
+        }
+      ],
+      "writers": [
+        {
+          "uuid": "a5a8f573-3cee-4ccc-8a2b-91cb9f55250a",
+          "full_name": "George Lucas"
+        }
+      ],
+      "directors": [
+        {
+          "uuid": "b505845f-d7bc-4271-a59d-5dc16a641cac",
+          "full_name": "Jon Burton"
+        }
+      ]
+    },
+    {
+      "uuid": "d4b010a5-2648-4850-b15d-307658020923",
+      "title": "Lego Star Wars: Revenge of the Brick",
+      "imdb_rating": 6.1,
+      "description": "Lego Anakin and Obi-Wan stop an invasion on Kashyyyk using the Force to influence their victory against the droid army.",
+      "genre": [
+        {
+          "name": "Animation",
+          "uuid": "6a0a479b-cfec-41ac-b520-41b2b007b611"
+        },
+        {
+          "name": "Comedy",
+          "uuid": "5373d043-3f41-4ea8-9947-4b746c601bbd"
+        },
+        {
+          "name": "Sci-Fi",
+          "uuid": "6c162475-c7ed-4461-9184-001ef3d9f26e"
+        },
+        {
+          "name": "Short",
+          "uuid": "a886d0ec-c3f3-4b16-b973-dedcf5bfa395"
+        }
+      ],
+      "actors": [],
+      "writers": [
+        {
+          "uuid": "80a599bd-80be-46f4-856c-8780f6694433",
+          "full_name": "Daniel Lipkowitz"
+        }
+      ],
+      "directors": [
+        {
+          "uuid": "26e83050-29ef-4163-a99d-b546cac208f8",
+          "full_name": "Mark Hamill"
+        },
+        {
+          "uuid": "438c4c36-c437-4d73-92e4-ca99583be4c9",
+          "full_name": "Karl Turkel"
+        },
+        {
+          "uuid": "6bd1f055-3eb0-4a6c-87e4-63b1794437a4",
+          "full_name": "Pete Bregman"
+        },
+        {
+          "uuid": "8e06c363-34b2-4671-98aa-48703b446a15",
+          "full_name": "Bill Horvath"
+        },
+        {
+          "uuid": "e7d7eebb-d55a-41ae-9eef-439aed13c2dc",
+          "full_name": "Royce Graham"
+        }
+      ]
+    },
+    {
+      "uuid": "87677f57-63bc-4498-a39c-4985a3b37643",
+      "title": "Lego Star Wars: The Yoda Chronicles",
+      "imdb_rating": 7.5,
+      "description": "Centered around Yoda's stories that take place before ''Lego Star Wars: The Padawan Menace''. Yoda begins by training Padawans at the Jedi Temple Academy, but then he feels a disturbance in the Force and rushes off to fight the Dark Side.",
+      "genre": [
+        {
+          "name": "Animation",
+          "uuid": "6a0a479b-cfec-41ac-b520-41b2b007b611"
+        },
+        {
+          "name": "Family",
+          "uuid": "55c723c1-6d90-4a04-a44b-e9792040251a"
+        },
+        {
+          "name": "Fantasy",
+          "uuid": "b92ef010-5e4c-4fd0-99d6-41b6456272cd"
+        },
+        {
+          "name": "Sci-Fi",
+          "uuid": "6c162475-c7ed-4461-9184-001ef3d9f26e"
+        }
+      ],
+      "actors": [
+        {
+          "uuid": "233f4bab-2a85-4567-aacf-f47a0b1f5b65",
+          "full_name": "Michael Donovan"
+        },
+        {
+          "uuid": "535fbcb6-2768-42c2-a3ab-878c7b2c42ab",
+          "full_name": "Trevor Devall"
+        },
+        {
+          "uuid": "5dc0293a-06ae-4b26-831b-7629b729a369",
+          "full_name": "Brian Dobson"
+        },
+        {
+          "uuid": "86f1f44b-39f5-41a6-8b3b-af5a9ed09858",
+          "full_name": "Anthony Daniels"
+        }
+      ],
+      "writers": [],
+      "directors": []
+    },
+    {
+      "uuid": "ee580283-32db-4181-9f11-bd9d171944c3",
+      "title": "Lego Star Wars: The Force Awakens",
+      "imdb_rating": 8.4,
+      "description": "30 years after the destructive Battle of Endor, a new threat emerges from the bricks of the old Empire, the First Order. Their plan: to end the New Republic and the Resistance, complete their ultimate weapon, and take over the galaxy. Luckily, hope is not lost. A scavenger, a rogue stormtrooper, and the Resistance's best pilot must come together with the aid of old allies to end the First Order's plans brick by brick. The most beloved LEGO video game franchise by Traveler's Tales returns to take you back to a long time ago in a galaxy far, far away with LEGO Star Wars: The Force Awakens - The Videogame!",
+      "genre": [
+        {
+          "name": "Adventure",
+          "uuid": "120a21cf-9097-479e-904a-13dd7198c1dd"
+        },
+        {
+          "name": "Sci-Fi",
+          "uuid": "6c162475-c7ed-4461-9184-001ef3d9f26e"
+        }
+      ],
+      "actors": [
+        {
+          "uuid": "27601239-80ae-4f84-b9b1-41a823fcb17e",
+          "full_name": "David Boat"
+        },
+        {
+          "uuid": "3e25cb48-1851-4286-b9be-b7f9d262bfa4",
+          "full_name": "David Acord"
+        },
+        {
+          "uuid": "73a1d3ee-064a-4ab5-9e11-abd77d833993",
+          "full_name": "Chopper Bernet"
+        },
+        {
+          "uuid": "84fbb03c-8672-41d6-a887-e1345f89db91",
+          "full_name": "JB Blanc"
+        }
+      ],
+      "writers": [
+        {
+          "uuid": "34e349f9-12a0-4ebe-8663-6c40baa200e5",
+          "full_name": "Graham Goring"
+        }
+      ],
+      "directors": [
+        {
+          "uuid": "4a2a3731-da3c-4575-b839-b4abe05939b8",
+          "full_name": "Pete Gomer"
+        },
+        {
+          "uuid": "c4975b6d-98cc-4354-818e-8dea5c47da14",
+          "full_name": "Jamie Eden"
+        },
+        {
+          "uuid": "cee7de69-f54c-41a5-9548-6c6946604246",
+          "full_name": "Robert Hewson"
+        }
+      ]
+    },
+    {
+      "uuid": "e99620fb-11bb-481b-8702-a14efa6bb0ef",
+      "title": "Lego Star Wars II: The Original Trilogy",
+      "imdb_rating": 8.4,
+      "description": "Play the original trilogy of star wars A New Hope,The Empire Strikes Back, and Return Of The Jedi. All in LEGO!!!!!!!!!",
+      "genre": [
+        {
+          "name": "Action",
+          "uuid": "3d8d9bf5-0d90-4353-88ba-4ccc5d2c07ff"
+        },
+        {
+          "name": "Adventure",
+          "uuid": "120a21cf-9097-479e-904a-13dd7198c1dd"
+        },
+        {
+          "name": "Sci-Fi",
+          "uuid": "6c162475-c7ed-4461-9184-001ef3d9f26e"
+        }
+      ],
+      "actors": [
+        {
+          "uuid": "5237aac5-f652-4aa5-9061-55bb007cd7be",
+          "full_name": "Tom Kane"
+        }
+      ],
+      "writers": [
+        {
+          "uuid": "a5a8f573-3cee-4ccc-8a2b-91cb9f55250a",
+          "full_name": "George Lucas"
+        },
+        {
+          "uuid": "f09cdc06-40bc-4ff4-b6a6-4da2dce6bbf2",
+          "full_name": "Jon M. Gibson"
+        }
+      ],
+      "directors": []
+    },
+    {
+      "uuid": "5d62b55c-1ed5-4563-ae80-10c4baa21a36",
+      "title": "Lego Star Wars III: The Clone Wars",
+      "imdb_rating": 7.5,
+      "description": "Anakin Skywalker and other Jedi fight their way through the Clone Wars in this interactive LEGO video game. You get to play through levels that make up seasons 1 & 2 of the TV series \"Star ...",
+      "genre": [
+        {
+          "name": "Sci-Fi",
+          "uuid": "6c162475-c7ed-4461-9184-001ef3d9f26e"
+        }
+      ],
+      "actors": [
+        {
+          "uuid": "3e25cb48-1851-4286-b9be-b7f9d262bfa4",
+          "full_name": "David Acord"
+        },
+        {
+          "uuid": "6c9f7977-fd5e-4990-b41b-9833efad1eea",
+          "full_name": "Ashley Eckstein"
+        },
+        {
+          "uuid": "a3468637-c3c3-442c-892e-1625385c49c6",
+          "full_name": "Dee Bradley Baker"
+        },
+        {
+          "uuid": "dbedbfbc-67c9-4637-a215-9cc861f3dd0b",
+          "full_name": "Corey Burton"
+        }
+      ],
+      "writers": [
+        {
+          "uuid": "a5a8f573-3cee-4ccc-8a2b-91cb9f55250a",
+          "full_name": "George Lucas"
+        }
+      ],
+      "directors": [
+        {
+          "uuid": "b505845f-d7bc-4271-a59d-5dc16a641cac",
+          "full_name": "Jon Burton"
+        }
+      ]
+    },
+    {
+      "uuid": "b8dfb15f-3060-4387-84a6-7ba3383589fe",
+      "title": "Lego Star Wars: The Yoda Chronicles - Secret Plans Are Revealed",
+      "imdb_rating": 6.5,
+      "description": "The adventures of Luke Skywalker and his friends whilst Luke is still in training, told in Lego.",
+      "genre": [
+        {
+          "name": "Animation",
+          "uuid": "6a0a479b-cfec-41ac-b520-41b2b007b611"
+        },
+        {
+          "name": "Short",
+          "uuid": "a886d0ec-c3f3-4b16-b973-dedcf5bfa395"
+        }
+      ],
+      "actors": [
+        {
+          "uuid": "233f4bab-2a85-4567-aacf-f47a0b1f5b65",
+          "full_name": "Michael Donovan"
+        },
+        {
+          "uuid": "45c40980-7bce-4e18-a0f8-e01deb83e0fa",
+          "full_name": "Kirby Morrow"
+        },
+        {
+          "uuid": "535fbcb6-2768-42c2-a3ab-878c7b2c42ab",
+          "full_name": "Trevor Devall"
+        },
+        {
+          "uuid": "f85e4336-828c-4228-ba7e-923348bb67a4",
+          "full_name": "Peter Kelamis"
+        }
+      ],
+      "writers": [
+        {
+          "uuid": "7fb5cb86-4b5d-4f78-97e3-6295ce2c0403",
+          "full_name": "Martin Skov"
+        },
+        {
+          "uuid": "80a599bd-80be-46f4-856c-8780f6694433",
+          "full_name": "Daniel Lipkowitz"
+        },
+        {
+          "uuid": "95567187-6aec-4d29-8d48-ea32a1db38a1",
+          "full_name": "John McCormack"
+        }
+      ],
+      "directors": [
+        {
+          "uuid": "7fb5cb86-4b5d-4f78-97e3-6295ce2c0403",
+          "full_name": "Martin Skov"
+        }
+      ]
+    },
+    {
+      "uuid": "aa90ffe4-6fc2-4cb9-b984-0cd169aedcd9",
+      "title": "Lego Star Wars: The Padawan Menace",
+      "imdb_rating": 7.2,
+      "description": "Ian,a young boy,Commander Cody and Yoda must go to rescue the secret battle plans.However,when substitute teacher C-3PO and a class of padawans go too,things become more complicated....",
+      "genre": [
+        {
+          "name": "Action",
+          "uuid": "3d8d9bf5-0d90-4353-88ba-4ccc5d2c07ff"
+        },
+        {
+          "name": "Adventure",
+          "uuid": "120a21cf-9097-479e-904a-13dd7198c1dd"
+        },
+        {
+          "name": "Animation",
+          "uuid": "6a0a479b-cfec-41ac-b520-41b2b007b611"
+        },
+        {
+          "name": "Comedy",
+          "uuid": "5373d043-3f41-4ea8-9947-4b746c601bbd"
+        },
+        {
+          "name": "Family",
+          "uuid": "55c723c1-6d90-4a04-a44b-e9792040251a"
+        },
+        {
+          "name": "Fantasy",
+          "uuid": "b92ef010-5e4c-4fd0-99d6-41b6456272cd"
+        },
+        {
+          "name": "Sci-Fi",
+          "uuid": "6c162475-c7ed-4461-9184-001ef3d9f26e"
+        },
+        {
+          "name": "Short",
+          "uuid": "a886d0ec-c3f3-4b16-b973-dedcf5bfa395"
+        }
+      ],
+      "actors": [
+        {
+          "uuid": "5237aac5-f652-4aa5-9061-55bb007cd7be",
+          "full_name": "Tom Kane"
+        },
+        {
+          "uuid": "6f685ee1-95cb-46e8-8e88-48f61c7b58ce",
+          "full_name": "Phil LaMarr"
+        },
+        {
+          "uuid": "86f1f44b-39f5-41a6-8b3b-af5a9ed09858",
+          "full_name": "Anthony Daniels"
+        },
+        {
+          "uuid": "e94a4fc6-af54-41c3-86cb-5cd9ef015f9b",
+          "full_name": "Nika Futterman"
+        }
+      ],
+      "writers": [
+        {
+          "uuid": "4ca937e0-34f6-4d56-8674-365be5a26a63",
+          "full_name": "Jens Nygaard Knudsen"
+        },
+        {
+          "uuid": "59711626-c533-4ef7-80c9-74df1ee983e0",
+          "full_name": "Godtfred Kirk Christiansen"
+        },
+        {
+          "uuid": "83b8d23a-2c8b-4c98-b6d4-6e2d2deba0b1",
+          "full_name": "Michael Price"
+        },
+        {
+          "uuid": "cdd472c9-bf4a-48c5-aa3a-099ec0089af2",
+          "full_name": "Ole Kirk Christiansen"
+        }
+      ],
+      "directors": [
+        {
+          "uuid": "b721075f-2ef0-4774-b362-9c110ee5d437",
+          "full_name": "David Scott"
+        }
+      ]
+    },
+    {
+      "uuid": "754589c1-e304-4ed8-87bc-1ac897529b97",
+      "title": "Lego Star Wars: The Freemaker Adventures",
+      "imdb_rating": 7.5,
+      "description": "The series stars the Freemakers, a family of scavengers who build and sell starships from the scoured debris of space battles strewn throughout the galaxy. When their youngest discovers a natural connection with the Force through an ancient artifact . The Kyber Saber, his world is turned upside down, and he and his family are thrown into an epic struggle against the Empire to restore peace and freedom to the galaxy. Throughout their adventures, the Freemakers explore new worlds, meet new and familiar characters, and learn the true value of what it means to be a family.",
+      "genre": [
+        {
+          "name": "Action",
+          "uuid": "3d8d9bf5-0d90-4353-88ba-4ccc5d2c07ff"
+        },
+        {
+          "name": "Animation",
+          "uuid": "6a0a479b-cfec-41ac-b520-41b2b007b611"
+        },
+        {
+          "name": "Comedy",
+          "uuid": "5373d043-3f41-4ea8-9947-4b746c601bbd"
+        },
+        {
+          "name": "Fantasy",
+          "uuid": "b92ef010-5e4c-4fd0-99d6-41b6456272cd"
+        },
+        {
+          "name": "Sci-Fi",
+          "uuid": "6c162475-c7ed-4461-9184-001ef3d9f26e"
+        }
+      ],
+      "actors": [
+        {
+          "uuid": "05cece18-8d6b-4178-89e4-2af0e1afc902",
+          "full_name": "Matthew Wood"
+        },
+        {
+          "uuid": "0e75f3ca-8eaf-4fae-9364-52340868f079",
+          "full_name": "Vanessa Lengies"
+        },
+        {
+          "uuid": "33941219-c386-4c33-a3fd-b3f6d8654607",
+          "full_name": "Nicolas Cantu"
+        },
+        {
+          "uuid": "5e5b87fa-e698-4ec0-8656-97b649f81a08",
+          "full_name": "Eugene Byrd"
+        }
+      ],
+      "writers": [
+        {
+          "uuid": "6ba834a0-1039-4b5c-ab78-d5f1f18a4e7d",
+          "full_name": "Carrie Beck"
+        },
+        {
+          "uuid": "6e50ec85-bd49-45e6-9f2c-cd22a828c973",
+          "full_name": "Bob Roth"
+        },
+        {
+          "uuid": "76bf1d79-69da-48aa-8f02-05930d4449bf",
+          "full_name": "Jason Cosler"
+        },
+        {
+          "uuid": "79a4ab94-9062-4ac8-aa31-7236ba838ae9",
+          "full_name": "Leland Chee"
+        },
+        {
+          "uuid": "948d3351-ee5c-48c2-9553-3acd196e7e6b",
+          "full_name": "Bill Motz"
+        },
+        {
+          "uuid": "95567187-6aec-4d29-8d48-ea32a1db38a1",
+          "full_name": "John McCormack"
+        },
+        {
+          "uuid": "d1591574-8b7d-4436-b633-444e49926553",
+          "full_name": "Keith Malone"
+        },
+        {
+          "uuid": "e86eb86b-4115-4dbb-b499-3086e849f36b",
+          "full_name": "Jake Blais"
+        }
+      ],
+      "directors": []
+    }
+  ]
