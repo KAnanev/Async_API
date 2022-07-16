29ca7266e0347b72cc54e15b3e325d920863ceca
tymv <tumkir@ya.ru>
2022-07-16 14:01:32 +0300

    Тест кэша Redis

--- src/tests/functional/src/test_films.py
+++ src/tests/functional/src/test_films.py
@@ -3,11 +3,13 @@ import random
 from uuid import uuid4
 
 import pytest
-from test_data.movies_data import MOVIES
+
+from ..test_data.movies_data import MOVIES
+from ..utils.es_loader import delete_index
 
 
 @pytest.mark.asyncio
-async def test_random_existent_film(load_movies_data, make_get_request):
+async def test_random_existent_film(load_movies_data, make_get_request, clear_redis_cashe):
     random_film = MOVIES[random.randint(0, len(MOVIES)-1)]
 
     response = await make_get_request(f'/films/{random_film["uuid"]}')
@@ -60,10 +62,16 @@ async def test_films_endpoint(load_movies_data, make_get_request):
     assert len(response.body) == len(MOVIES)
     assert response.body == MOVIES
 
-# @pytest.mark.asyncio
-# async def test_without_index(clear_redis_cashe, make_get_request):
-#     response = await make_get_request('/films')
 
-#     assert response.status == 200
-#     assert len(response.body) == len(MOVIES)
-#     assert response.body == MOVIES
+@pytest.mark.asyncio
+async def test_cache_without_index(es_client, clear_redis_cashe, make_get_request):
+    random_film = MOVIES[random.randint(0, len(MOVIES)-1)]
+
+    response_from_elastic = await make_get_request(f'/films/{random_film["uuid"]}')
+    await delete_index(es_client, 'movies')
+
+    response_from_redis = await make_get_request(f'/films/{random_film["uuid"]}')
+
+    assert response_from_elastic.status == 200
+    assert response_from_redis.status == 200
+    assert response_from_elastic.body == response_from_redis.body == random_film
